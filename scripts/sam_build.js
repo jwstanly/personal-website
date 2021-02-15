3d60107485c38e47c23dec1a974353b5dfa148b5
jwstanly <jwstanly@yahoo.com>
2021-02-14 21:07:55 -0500

    sam auto find template; rerouting sam handlers in template

--- scripts/sam_build.js
+++ scripts/sam_build.js
@@ -7,6 +7,6 @@ Create an S3 Bucket and CloudFront Distribution that serves up a React app at a 
 (function() {
    sh.echo(`Building a Bucket and SAM distribution hosted at "${BUCKET_NAME}"`);
    sh.exec(
-      `sam build --template-file ./template.yml --parameter-overrides DomainName=${BUCKET_NAME} FullDomainName=www.${BUCKET_NAME} AcmCertificateArn=${ACM_CERT_ARN}`
+      `sam build --parameter-overrides DomainName=${BUCKET_NAME} FullDomainName=www.${BUCKET_NAME} AcmCertificateArn=${ACM_CERT_ARN}`
    );
 })();
