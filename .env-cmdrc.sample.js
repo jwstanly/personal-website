b8c1d4af7eca2ed8da013b20c368eb8d5a3d1477
jwstanly <jwstanly@yahoo.com>
2021-02-15 15:23:13 -0500

    realizing my bucket sample was wrong

--- .env-cmdrc.sample.js
+++ .env-cmdrc.sample.js
@@ -2,7 +2,7 @@ const env = {
    production: {
       REACT_APP_ENV: 'PRODUCTION',
       REACT_APP_ClientUrl: 'www.jwstanly.com',
-      BUCKET_NAME: 'jwstanly-bucket',
+      BUCKET_NAME: 'jwstanly.com',
       ACM_CERT_ARN: 'REPLACE_ME',
       CF_DISTRIBUTION_ID: 'REPLACE_ME',
    },
