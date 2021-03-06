17beb7cb418af8f05286f620890f8dc8da160211
jwstanly <jwstanly@yahoo.com>
2021-03-05 21:37:34 -0500

    adding Montserrat font

---
+++ pages/_document.js
@@ -0,0 +1,33 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+
+        <link 
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+        />
+        <link 
+          href='https://fonts.googleapis.com/css?family=Montserrat:thin,extra-light,light,100,200,300,400,500,600,700,800'
+          rel='stylesheet'
+          type='text/css'
+        />
+
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
