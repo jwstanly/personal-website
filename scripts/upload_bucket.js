4574bb5a592347fe2ca7978b82917e5b7c7550b3
jwstanly <jwstanly@yahoo.com>
2021-03-10 21:21:23 -0500

    revised syntax in upload bucket script

--- scripts/upload_bucket.js
+++ scripts/upload_bucket.js
@@ -1,18 +1,12 @@
 const sh = require('shelljs');
 const { AWS_CLI_PROFILE, REACT_APP_ENV, BUCKET_NAME, CF_DISTRIBUTION_ID } = process.env;
 
-/**
- * Upload the /build folder contents to your production S3 Bucket
- */
-let bucket = BUCKET_NAME,
-   distribution = CF_DISTRIBUTION_ID;
+const bucket = BUCKET_NAME;
+const distribution = CF_DISTRIBUTION_ID;
 
+sh.echo(`Deploying the ${REACT_APP_ENV} build to Bucket ${bucket}`);
+sh.exec(`aws s3 sync out s3://${bucket} --delete --profile ${AWS_CLI_PROFILE}`);
 
-(function() {
-   sh.echo(`Deploying the ${REACT_APP_ENV} build to Bucket ${bucket}`);
-   sh.exec(`aws s3 sync out s3://${bucket} --delete --profile ${AWS_CLI_PROFILE}`);
-
-   if (distribution) {
-      sh.exec(`aws cloudfront create-invalidation --distribution-id ${distribution} --paths "/*" --profile ${AWS_CLI_PROFILE}`);
-   }
-})();
+if (distribution) {
+   sh.exec(`aws cloudfront create-invalidation --distribution-id ${distribution} --paths "/*" --profile ${AWS_CLI_PROFILE}`);
+}
