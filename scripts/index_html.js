bf908ab037fab5e2b24de954eab223e3cf355a7c
jwstanly <jwstanly@yahoo.com>
2021-03-10 20:06:17 -0500

    added script for properly indexing html files for s3 website

---
+++ scripts/index_html.js
@@ -0,0 +1,21 @@
+const shell = require('shelljs');
+
+shell.echo(`Placing HTML files into folders so S3 deployment can properly index them`);
+
+shell.cd('out');
+shell.ls('-R','**/*.html').forEach(file => {
+  
+  const pathArray = file.split('/').slice(0, -1); // [blog,post]
+  const fileName = file.split('/').pop(); // blog.html
+
+  if(fileName !== 'index.html') {
+
+    const pathToNewDir = 
+      `./${pathArray.length ? pathArray.join('/') : ''}/${fileName.split('.')[0]}`;
+
+    shell.mkdir(pathToNewDir);
+    shell.cp(file, `${pathToNewDir}/index.html`)
+
+  }
+});
+shell.cd('..');
