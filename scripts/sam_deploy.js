b260e0fbc87228c046715c7e284eaf147c37c837
jwstanly <jwstanly@yahoo.com>
2021-03-10 21:22:30 -0500

    added region control in upload bucket script

--- scripts/sam_deploy.js
+++ scripts/sam_deploy.js
@@ -1,8 +1,7 @@
 const sh = require('shelljs');
 
 const { AWS_CLI_PROFILE, BUCKET_NAME, ACM_CERT_ARN, AWS_REGION } = process.env;
-const region = AWS_REGION ? AWS_REGION : String(shell.exec('aws configure get region'));
-
+  
 sh.echo(`Deploying resources for "${BUCKET_NAME}"`);
 sh.exec(
    `sam deploy --stack-name jwstanly-website --parameter-overrides DomainName=${BUCKET_NAME} AcmCertificateArn=${ACM_CERT_ARN} AWSRegion=${region} --resolve-s3 --capabilities CAPABILITY_IAM --profile ${AWS_CLI_PROFILE} --region ${region}`
--- scripts/upload_bucket.js
+++ scripts/upload_bucket.js
@@ -1,12 +1,13 @@
 const sh = require('shelljs');
-const { AWS_CLI_PROFILE, REACT_APP_ENV, BUCKET_NAME, CF_DISTRIBUTION_ID } = process.env;
+const { AWS_CLI_PROFILE, AWS_REGION, REACT_APP_ENV, BUCKET_NAME, CF_DISTRIBUTION_ID } = process.env;
 
 const bucket = BUCKET_NAME;
 const distribution = CF_DISTRIBUTION_ID;
+const region = AWS_REGION ? AWS_REGION : String(shell.exec('aws configure get region'));
 
 sh.echo(`Deploying the ${REACT_APP_ENV} build to Bucket ${bucket}`);
 sh.exec(`aws s3 sync out s3://${bucket} --delete --profile ${AWS_CLI_PROFILE}`);
 
 if (distribution) {
-   sh.exec(`aws cloudfront create-invalidation --distribution-id ${distribution} --paths "/*" --profile ${AWS_CLI_PROFILE}`);
+   sh.exec(`aws cloudfront create-invalidation --distribution-id ${distribution} --paths "/*" --profile ${AWS_CLI_PROFILE} --region ${region}`);
 }
