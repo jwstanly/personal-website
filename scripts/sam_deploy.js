44eec74d998d2efe61e858622770c955855fcd0c
jwstanly <jwstanly@yahoo.com>
2021-03-12 20:34:55 -0500

    somehow regions portion removed from deploy script; redadded

--- scripts/sam_deploy.js
+++ scripts/sam_deploy.js
@@ -1,6 +1,7 @@
 const sh = require('shelljs');
 
 const { AWS_CLI_PROFILE, BUCKET_NAME, ACM_CERT_ARN, AWS_REGION } = process.env;
+const region = AWS_REGION ? AWS_REGION : String(shell.exec('aws configure get region'));
   
 sh.echo(`Deploying resources for "${BUCKET_NAME}"`);
 sh.exec(
