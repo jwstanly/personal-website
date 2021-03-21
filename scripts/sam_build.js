08f8c04b5cdf301bbfeb9d52fbdd58dccd0f9789
jwstanly <jwstanly@yahoo.com>
2021-03-21 13:14:53 -0400

    removing AWSRegion CF var in favor of AWS::Region pseudo var

--- scripts/sam_build.js
+++ scripts/sam_build.js
@@ -1,9 +1,16 @@
 const sh = require('shelljs');
 
-const { AWS_CLI_PROFILE, BUCKET_NAME, ACM_CERT_ARN, AWS_REGION } = process.env;
-const region = AWS_REGION ? AWS_REGION : String(shell.exec('aws configure get region'));
+const { 
+   AWS_CLI_PROFILE, 
+   BUCKET_NAME, 
+   ACM_CERT_ARN, 
+} = process.env;
 
 sh.echo(`Building SAM resources for "${BUCKET_NAME}"`);
 sh.exec(
-   `sam build --parameter-overrides DomainName=${BUCKET_NAME} AcmCertificateArn=${ACM_CERT_ARN} AWSRegion=${region} --profile ${AWS_CLI_PROFILE} --region ${region}`
+   'sam build '
+   + '--parameter-overrides '
+      + `DomainName=${BUCKET_NAME} `
+      + `AcmCertificateArn=${ACM_CERT_ARN} `
+   + `--profile ${AWS_CLI_PROFILE} `
 );
--- scripts/sam_deploy.js
+++ scripts/sam_deploy.js
@@ -1,9 +1,19 @@
 const sh = require('shelljs');
 
-const { AWS_CLI_PROFILE, BUCKET_NAME, ACM_CERT_ARN, AWS_REGION } = process.env;
-const region = AWS_REGION ? AWS_REGION : String(shell.exec('aws configure get region'));
+const { 
+   AWS_CLI_PROFILE, 
+   BUCKET_NAME, 
+   ACM_CERT_ARN,
+} = process.env;
   
 sh.echo(`Deploying resources for "${BUCKET_NAME}"`);
 sh.exec(
-   `sam deploy --stack-name jwstanly-website --parameter-overrides DomainName=${BUCKET_NAME} AcmCertificateArn=${ACM_CERT_ARN} AWSRegion=${region} --resolve-s3 --capabilities CAPABILITY_IAM --profile ${AWS_CLI_PROFILE} --region ${region}`
+   'sam deploy '
+   + `--stack-name jwstanly-website `
+   + '--parameter-overrides '
+      + `DomainName=${BUCKET_NAME} `
+      + `AcmCertificateArn=${ACM_CERT_ARN} `
+   + '--resolve-s3 '
+   + '--capabilities CAPABILITY_IAM '
+   + `--profile ${AWS_CLI_PROFILE} `
 );
