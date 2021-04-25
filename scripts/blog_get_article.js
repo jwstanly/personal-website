1551ab225826d894b655e2caac557cbacbaefa9e
jwstanly <jwstanly@yahoo.com>
2021-04-25 16:23:17 -0400

    added basic CMS with scripts to get and post blog articles

---
+++ scripts/blog_get_article.js
@@ -0,0 +1,33 @@
+const input = require('prompt-sync')();
+const fs = require('fs');
+const https = require('https');
+const fetch = require('node-fetch');
+
+const { DOMAIN_NAME, API_KEY } = process.env;
+
+console.log("Get blog article")
+
+const title = input('Title: ');
+const contentsFile = input('Ouput contents file [./edit.md]: ');
+
+console.log("Fetching article...");
+
+fetch(`https://api.${DOMAIN_NAME}/blog?title=${title.split(" ").join("+")}`, {
+  method: 'GET',
+  headers: {
+    'Accept': 'application/json',
+    'X-API-KEY': API_KEY,
+    'Content-Type': 'application/json',
+  },
+})
+  .then(res => res.json())
+  .then(res => {
+    fs.writeFileSync(
+      contentsFile.length ? contentsFile : './edit.md',
+      res.Item.content 
+    );
+    console.log(`SUCCESS: ${title} was fetched to ${contentsFile.length ? contentsFile : './edit.md'}`);
+  })
+  .catch(error => {
+    console.log("ERROR: Failed to publish blog article:\n", error);
+  });
---
+++ scripts/blog_post_article.js
@@ -0,0 +1,93 @@
+const input = require('prompt-sync')();
+const fs = require('fs');
+const https = require('https');
+const fetch = require('node-fetch');
+const { spawn } = require('child_process')
+
+const { DOMAIN_NAME, API_KEY } = process.env;
+const TAG_DELIMITER = ', ';
+
+async function publishArticle() {
+
+  console.log("Publish a new blog article")
+
+  const title = input('Title: ');
+
+  // Grab the article's data if the article already exists
+  const currentArticle = await fetch(`https://api.${DOMAIN_NAME}/blog?title=${title.split(" ").join("+")}`, {
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json',
+      'X-API-KEY': API_KEY,
+      'Content-Type': 'application/json',
+    },
+  })
+    .then(res => res.json())
+    .then(res => res.Item)
+    .catch(() => {})
+
+  const subheader = input(`Subheader${currentArticle ? ` [${currentArticle.subheader}]` : ''}: `);
+  const image = input(`Image${currentArticle ? ` [${currentArticle.image}]` : ''}: `);
+  const tags = input(`Tags (split by \`${TAG_DELIMITER}\`)${currentArticle ? ` [${currentArticle.tags.join(TAG_DELIMITER)}]` : ''}: `);
+
+  const contentsFile = input('Contents file [./edit.md]: ');
+
+  const contents = fs.readFileSync(
+    contentsFile.length ? contentsFile : './edit.md', 
+    'utf8'
+  );
+
+  const newArticle = {
+    title: title.split("+").join(" "), 
+    subheader: subheader.length ? subheader : currentArticle.subheader,
+    image: image.length ? image : currentArticle.image,
+    tags: tags.length ? tags.split(TAG_DELIMITER) : currentArticle.tags,
+    content: contents,
+  }
+
+  console.log("Article JSON:\n", newArticle);
+  const publishConfirm = input('Publish? [Y/n]: ');
+
+  if (publishConfirm === 'n' || publishConfirm === 'N') {
+    console.log("Publish cancelled");
+    process.exit(0);
+  } 
+
+  console.log("Publishing...");
+
+  await fetch(`https://api.${DOMAIN_NAME}/blog?title=${title.split(" ").join("+")}`, {
+      headers: {
+        'Accept': 'application/json',
+        'X-API-KEY': API_KEY,
+        'Content-Type': 'application/json',
+      },
+      method: "POST",
+      body: JSON.stringify(newArticle),
+    })
+      .then(res => {
+        console.log("SUCCESS: Blog article published");
+      })
+      .catch(error => {
+        console.log("ERROR: Failed to publish blog article:\n", error);
+      });
+  
+  const deployFrontend = input('Deploy frontend? [Y/n]: ');
+
+  if (deployFrontend === 'n' || deployFrontend === 'N') {
+    console.log("Frontend not deployed");
+    process.exit(0);
+  } 
+
+  const deployment = spawn("yarn", ["deploy:frontend"]);
+      
+  deployment.stdout.on('data', data => {
+    console.log(data.toString());
+  });
+
+  deployment.on('exit', code => {
+    console.log('SUCCESS: New frontend deployaed');
+  });
+
+}
+
+publishArticle();
