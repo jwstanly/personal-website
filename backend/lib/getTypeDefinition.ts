cc25e4583787c008813b9f5946dc6d91756f5487
jwstanly <jwstanly@yahoo.com>
2021-12-31 22:42:05 -0500

    remove now unused files

--- backend/lib/getTypeDefinition.ts
+++
@@ -1,5 +0,0 @@
-import schema from '../types.schema.json';
-
-export default function getTypeDefinition(type: string | object): object {
-  return typeof type === 'object' ? type : schema.definitions[type];
-}
