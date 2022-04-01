d3f7194eecf48711480026fe8c92518c8c236324
jwstanly <jwstanly@yahoo.com>
2022-04-01 13:05:59 -0400

    silly tailwind

--- components/LargeIcon.tsx
+++ components/LargeIcon.tsx
@@ -9,7 +9,11 @@ export function LargeIcon(props: LargeIconProps) {
   return (
     <img
       className="rounded-3xl w-36 h-36 max-w-xl m-0 mt-2 aspect-square"
-      style={{ boxShadow: '0px 0px 40px 5px #CCC' }}
+      style={{
+        boxShadow: '0px 0px 40px 5px #CCC',
+        height: '9rem',
+        width: '9rem',
+      }}
       src={props.imageUrl}
       alt={props.alt}
     />
