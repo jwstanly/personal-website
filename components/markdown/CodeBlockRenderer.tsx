1b28156517857f8588b76e37408681a2032f42f8
jwstanly <jwstanly@yahoo.com>
2022-04-21 11:15:16 -0400

    code block style changes

--- components/markdown/CodeBlockRenderer.tsx
+++ components/markdown/CodeBlockRenderer.tsx
@@ -7,23 +7,25 @@ interface CodeBlockRendererProps {
 
 export default function CodeBlockRenderer(props: CodeBlockRendererProps) {
   return (
-    <SyntaxHighlighter
-      language={props.language}
-      style={style}
-      showLineNumbers={false}
-      wrapLines={true}
-      customStyle={
-        {
-          fontSize: 14,
-          padding: 20,
-          borderRadius: 8,
-          marginTop: 25,
-          marginBottom: 25,
-        } as React.CSSProperties
-      }
-      useInlineStyles={true}
-    >
-      {props.value}
-    </SyntaxHighlighter>
+    <div className="shadow-md rounded-lg border-0">
+      <SyntaxHighlighter
+        language={props.language}
+        style={style}
+        showLineNumbers={false}
+        wrapLines={true}
+        customStyle={
+          {
+            fontSize: 14,
+            padding: 20,
+            borderRadius: 8,
+            marginTop: 25,
+            marginBottom: 25,
+          } as React.CSSProperties
+        }
+        useInlineStyles={true}
+      >
+        {props.value}
+      </SyntaxHighlighter>
+    </div>
   );
 }
--- components/markdown/CodeInlineRenderer.tsx
+++ components/markdown/CodeInlineRenderer.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import style from 'react-syntax-highlighter/dist/cjs/styles/prism/vsc-dark-plus';
+// import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+// import style from 'react-syntax-highlighter/dist/cjs/styles/prism/prism';
 
 interface CodeInlineRendererProps {
   children: string;
@@ -12,34 +12,36 @@ interface CodeInlineRendererProps {
 
 export default function CodeInlineRenderer(props: CodeInlineRendererProps) {
   return (
-    <SyntaxHighlighter
-      language="javascript"
-      style={style}
-      showLineNumbers={false}
-      wrapLines={true}
-      customStyle={
-        {
-          wordWrap: 'unset',
-          margin: 0,
-        } as React.CSSProperties
-      }
-      useInlineStyles={true}
-      PreTag={p => (
-        <span
-          {...p}
-          style={{
-            lineHeight: '0.5em',
-            fontSize: '90%',
-            padding: '0.1em 0.3em 0.2em',
-            margin: '0 0.05em',
-            borderRadius: '0.3em',
-            backgroundColor: style['pre[class*="language-"]'].background,
-          }}
-        />
-      )}
-      CodeTag={p => <span {...p} />}
-    >
-      {props.value}
-    </SyntaxHighlighter>
+    <code>{props.children}</code>
+    // <SyntaxHighlighter
+    //   language="none"
+    //   style={style}
+    //   showLineNumbers={false}
+    //   wrapLines={true}
+    //   // customStyle={}
+    //   // codeTagProps={{
+    //   //   style: {
+    //   //     wordWrap: 'break-word',
+    //   //     margin: 0,
+    //   //   } as React.CSSProperties,
+    //   // }}
+    //   useInlineStyles={true}
+    //   PreTag={p => (
+    //     <span
+    //       {...p}
+    //       style={{
+    //         lineHeight: '0.5em',
+    //         fontSize: '90%',
+    //         padding: '0.1em 0.3em 0.2em',
+    //         margin: '0 0.05em',
+    //         borderRadius: '0.3em',
+    //         backgroundColor: style['pre[class*="language-"]'].background,
+    //       }}
+    //     />
+    //   )}
+    //   CodeTag={p => <code {...p} style={{ wordWrap: 'break-word' }} />}
+    // >
+    //   {props.value}
+    // </SyntaxHighlighter>
   );
 }
