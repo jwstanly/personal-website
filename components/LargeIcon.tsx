a210298c96eff26602c03152510acace36096af1
jwstanly <jwstanly@yahoo.com>
2022-08-12 12:28:52 -0400

    fix aspect ratio 1:1 issue

--- components/LargeIcon.tsx
+++ components/LargeIcon.tsx
@@ -8,7 +8,7 @@ interface LargeIconProps {
 export function LargeIcon(props: LargeIconProps) {
   return (
     <img
-      className="rounded-3xl w-36 h-36 max-w-xl m-0 mt-2 aspect-square"
+      className="rounded-3xl w-36 h-36 max-w-xl m-0 mt-2 object-cover"
       style={{
         boxShadow: '0px 0px 40px 5px #CCC',
         height: '9rem',
