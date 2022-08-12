c43c57b02b60f9d57fef57c6751ab16dee2e95b8
jwstanly <jwstanly@yahoo.com>
2022-08-11 21:37:09 -0400

    add cache policy for images

---
+++ next.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: '/images/:slug*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, s-maxage=10, stale-while-revalidate=59',
+          },
+        ],
+      },
+      {
+        source: '/:slug*.(gif|jpe?g|tiff?|png|webp|bmp)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, s-maxage=10, stale-while-revalidate=59',
+          },
+        ],
+      },
+    ];
+  },
+};
