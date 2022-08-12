a476ff2acb7be5a816bc27c71dd9acfa5f8d55fb
jwstanly <jwstanly@yahoo.com>
2022-08-12 12:29:12 -0400

    load fonts in head

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -30,24 +30,23 @@ class MyDocument extends Document {
           `,
             }}
           />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css?family=Montserrat:thin,extra-light,light,100,200,300,400,500,600,700,800"
+            rel="stylesheet"
+            type="text/css"
+          />
+          <link
+            href="https://fonts.googleapis.com/css?family=Inter:thin,extra-light,light,100,200,300,400,500,600,700,800"
+            rel="stylesheet"
+            type="text/css"
+          />
+          <link
+            href="https://fonts.googleapis.com/css?family=IBM+Plex+Mono"
+            rel="stylesheet"
+            type="text/css"
+          />
         </Head>
-
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css?family=Montserrat:thin,extra-light,light,100,200,300,400,500,600,700,800"
-          rel="stylesheet"
-          type="text/css"
-        />
-        <link
-          href="https://fonts.googleapis.com/css?family=Inter:thin,extra-light,light,100,200,300,400,500,600,700,800"
-          rel="stylesheet"
-          type="text/css"
-        />
-        <link
-          href="https://fonts.googleapis.com/css?family=IBM+Plex+Mono"
-          rel="stylesheet"
-          type="text/css"
-        />
 
         <body>
           <Main />
