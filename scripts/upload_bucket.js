cb602fac041c6b6e93eede784a2504f88e44076e
jwstanly <jwstanly@yahoo.com>
2022-08-24 09:57:27 -0400

    improve s3 uploading to not purge files

--- scripts/upload_bucket.js
+++ scripts/upload_bucket.js
@@ -2,18 +2,22 @@ const sh = require('shelljs');
 const { AWS_CLI_PROFILE, ENV_NAME, DOMAIN_NAME, CF_DISTRIBUTION_ID } =
   process.env;
 
+const excludedPaths = ['images', 'files', 'resume.pdf'];
+
 sh.echo(`Deploying the ${ENV_NAME} build to Bucket ${DOMAIN_NAME}`);
 
 sh.exec(
   `aws s3 sync out s3://${DOMAIN_NAME} ` +
     '--delete ' +
-    '--exclude /images --exclude /resume.pdf ' +
+    excludedPaths.map(p => `--exclude ${p}`).join(' ') +
     `--profile ${AWS_CLI_PROFILE} `,
 );
 
-sh.exec(
-  `aws s3 cp out/images s3://${DOMAIN_NAME}/images --recursive --profile ${AWS_CLI_PROFILE} `,
-);
+for (const path of excludedPaths) {
+  sh.exec(
+    `aws s3 cp out/${path} s3://${DOMAIN_NAME}/${path} --recursive --profile ${AWS_CLI_PROFILE} `,
+  );
+}
 
 if (CF_DISTRIBUTION_ID) {
   sh.echo('\n');
